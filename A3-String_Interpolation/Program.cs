d6d48e658072d0f89e7863f2f3cec7b828bdb466
cmoldenauer <97994521+cmoldenauer@users.noreply.github.com>
2022-08-31 21:03:35 -0500

    added String Interpolation

--- A3-String_Interpolation/Program.cs
+++ A3-String_Interpolation/Program.cs
@@ -5,8 +5,18 @@
         static void Main(string[] args)
         {
             var date = DateTime.Now;
-            Console.WriteLine($"{date:MMMM d yyyy}");
+            Console.WriteLine($"{date,40:MMMM d yyyy}");
             Console.WriteLine($"{date:yyyy.MM.dd}");
+            Console.WriteLine($"Day {date:d} of {date:MMMM, yyyy}");
+            Console.WriteLine($"Year: {date:yyyy}, Month: {date:MM}, Day: {date:dd}");
+            Console.WriteLine($"{date,10:dddd}");
+            Console.WriteLine($"{date,10:hh:mm tt}{date,10:dddd}");
+            Console.WriteLine($"h:{date:hh}, m:{date:mm}, s:{date:ss}");
+            Console.WriteLine($"{date:yyyy.MM.dd.hh.mm.ss}");
+
+            var pi = Math.PI;
+            Console.WriteLine($"{pi:C}");
+            Console.WriteLine($"{pi,10:n3}");
         }
     }
 }
