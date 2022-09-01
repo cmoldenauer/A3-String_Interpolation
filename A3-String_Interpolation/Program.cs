a3d4dedb531f65ec1e25a1e305749e246726a1d3
cmoldenauer <97994521+cmoldenauer@users.noreply.github.com>
2022-08-31 20:31:34 -0500

    Add project files.

---
+++ A3-String_Interpolation/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace A3_String_Interpolation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var date = DateTime.Now;
+            Console.WriteLine($"{date:MMMM d yyyy}");
+            Console.WriteLine($"{date:yyyy.MM.dd}");
+        }
+    }
+}
